8bc242bad9070a714020a5593121d8d53cd1cc55
Victor Rodriguez <awtgerry@gmail.com>
2023-06-27 22:42:59 -0600

    tmp fix for network

--- .config/awesome/lua/awt/bar/network.lua
+++ .config/awesome/lua/awt/bar/network.lua
@@ -8,22 +8,24 @@ local function get_network(widget)
 
     local ethernet = io.popen("nmcli device status | grep ethernet | awk '{print $3}'")
     local ethernet_status = ethernet:read("*all")
-    ethernet:close()
+    -- ethernet:close()
 
     if string.find(ethernet_status, "connected") then
-        local ethernet = io.popen("nmcli device status | grep ethernet | awk '{print $1}'")
-        local ethernet_name = ethernet:read("*all")
-        ethernet:close()
-        str = "󰈀  " .. ethernet_name
+        -- local ethernet = io.popen("nmcli device status | grep ethernet | awk '{print $1}'")
+        -- local ethernet_name = ethernet:read("*all")
+        -- ethernet:close()
+        -- str = "󰈀 " .. ethernet_name
+        str = "󰈀 "
     else -- TODO: checar si funciona bien con wifi
         local wifi = io.popen("nmcli -t -f active,ssid dev wifi | grep yes | cut -c -3")
         local wifi_status = wifi:read("*all")
         wifi:close()
         if string.find(wifi_status, "yes") then
-            local con = io.popen("nmcli -t -f active,ssid dev wifi | grep yes | cut -c 5-")
-            local conname = con:read("*all")
-            conname:close()
-            str = "󰤨 " .. conname
+            -- local con = io.popen("nmcli -t -f active,ssid dev wifi | grep yes | cut -c 5-")
+            -- local conname = con:read("*all")
+            -- conname:close()
+            -- str = "󰤨 " .. conname
+            str = "󰤨 "
         else
             str = "󰤭 "
         end
