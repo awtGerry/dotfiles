0aa779b23d21b24c9d3794b78fd4d047450c0ae3
Victor Rodriguez <awtgerry@gmail.com>
2023-07-08 14:50:25 -0600

    mod nvim

--- .config/nvim/lua/packer/plugins/lsp.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "neovim/nvim-lspconfig",
-    "onsails/lspkind-nvim",
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    "folke/neodev.nvim",
-}
