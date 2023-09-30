c9d71ea0a704360cd6f430b97ea60ff3ccf77d83
Victor Rodriguez <awtgerry@gmail.com>
2023-09-30 11:46:51 -0600

    Kinda working
    TODO: Improve the strcuture of the program to add the other things.

--- .config/suckless/bar-rs/src/bar.rs
+++ .config/suckless/bar-rs/src/bar.rs
@@ -1,6 +1,35 @@
-use crate::components::date;
+use crate::components;
 
-// insert components/date.rs
+struct Bar {
+    display: *mut u8,
+    root: *mut u8,
+    command: String
+}
+
+#[allow(unused)]
+impl Bar {
+    fn new() -> Bar {
+        let display = unsafe { XOpenDisplay(0 as *const u8) };
+        let root = unsafe { XDefaultRootWindow(display) };
+
+        /* HERE WILL GO ALL THE COMPONENTS */
+        let date = components::date::get_date();
+        let time = components::date::get_time();
+
+        let command = format!("{}  {}", date, time);
+
+        Bar {
+            display,
+            root,
+            command
+        }
+    }
+
+    fn update(&mut self) {
+        unsafe { XStoreName(self.display, self.root, self.command.as_ptr()) };
+    }
+}
+
 #[link(name = "X11")]
 extern "C" {
     // https://www.x.org/releases/X11R7.5/doc/man/man3/XOpenDisplay.3.html
@@ -9,14 +38,24 @@ extern "C" {
     fn XStoreName(display: *mut u8, w: *mut u8, name: *const u8) -> i32;
 }
 
+// pub fn display_bar() {
+//     let mut bar = Bar::new();
+//     loop {
+//         bar.update();
+//         std::thread::sleep(std::time::Duration::from_nanos((1e9 / 144.) as u64));
+//     }
+// }
+
 pub fn display_bar() {
     let display = unsafe { XOpenDisplay(0 as *const u8) };
     let root = unsafe { XDefaultRootWindow(display) };
-    let date = date::get_date();
     loop {
-        unsafe { XStoreName(display, root, "Hi\0".as_ptr()) };
-        unsafe { XStoreName(display, root, date.as_ptr()) };
-    
+        let date = components::date::get_date();
+        let time = components::date::get_time();
+        let command = format!("{}  {}", date, time);
+
+        unsafe { XStoreName(display, root, command.as_ptr()) };
+
         std::thread::sleep(std::time::Duration::from_nanos((1e9 / 144.) as u64));
     }
 }
