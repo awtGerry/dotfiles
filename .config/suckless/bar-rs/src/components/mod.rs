4d844b2b37585b0eb083f11ba60076be002541c3
Victor Rodriguez <awtgerry@gmail.com>
2024-01-23 12:36:03 -0600

    remove suckless software from here.

--- .config/suckless/bar-rs/src/components/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod battery;
-pub mod date;
-pub mod test;
-pub mod layout;
-pub mod volume;
-pub mod network;
--- .config/suckless/bar-rs/src/main.rs
+++
@@ -1,6 +0,0 @@
-mod bar;
-mod components;
-
-fn main() {
-    bar::display_bar();
-}
